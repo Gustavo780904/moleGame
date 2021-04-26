f87fac11d6306844e28979c1f683693e21bc9945
Gustavo780904 <gustavodutrasantos@gmail.com>
2021-04-26 10:27:25 -0300

    finalizado

--- js/game.js
+++ js/game.js
@@ -1,14 +1,13 @@
 const $levels = { "easy": 3, "medium": 5, "hard": 7 }
 const $imgWidth = 100
 const $imgHeight = 80
-$(document).ready(function () {
-    fillboard();
-    $("#btnPlay").click(function () {
-       setInterval(startGame(),1180)
-
+$(document).ready(function() {
+        fillboard();
+        $("#btnPlay").click(function() {
+            setInterval(startGame, 1180);
+        })
     })
-})
-//cria o tabuleiro(moldura)conforme o nivel
+    //cria o tabuleiro(moldura)conforme o nivel
 function fillboard() {
     // alert($("#level").val())
     $level = $levels[$("#level").val()]
@@ -20,23 +19,26 @@ function fillboard() {
 //insere os buracos
 function placeHolesBoard($level) {
     $("#board").empty()
-    for ($i=0; $i<Math.pow($level,2); $i++) {
-        $div = $("<div></div>");//.attr("id", `mole_${$i+1}`);
-        $img = $("<img>").attr({"src":"img/buraco.gif", "id": `mole_${$i+1}`});
+    for ($i = 0; $i < Math.pow($level, 2); $i++) {
+        $div = $("<div></div>"); //.attr("id", `mole_${$i+1}`);
+        $img = $("<img>").attr({ "src": "img/buraco.gif", "id": `mole_${$i+1}` });
         $($div).append($img);
         $("#board").append($div);
     }
 }
-function startGame(){
+
+function startGame() {
     fillboard()
     $level = getLevel()
-    // $level = $levels[$("#level").val()]
+        // $level = $levels[$("#level").val()]
     $randNumber = getRandNumber(1, Math.pow($level, 2))
     $(`#mole_${$randNumber}`).attr("src", "img/toupeira.gif")
 }
+
 function getRandNumber(min, max) {
     return Math.round((Math.random() * Math.abs(max - min)) + min);
 }
-function getLevel(){
+
+function getLevel() {
     return $levels[$("#level").val()]
 }
