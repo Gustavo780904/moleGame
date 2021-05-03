7c554a0dd540a2e06cfed3af13bc7619aa7195a2
Gustavo780904 <gustavodutrasantos@gmail.com>
2021-05-03 11:41:40 -0300

    Etapa6

--- js/game.js
+++ js/game.js
@@ -1,6 +1,7 @@
 const $levels = { "easy": 3, "medium": 5, "hard": 7 }
 const $imgWidth = 100
 const $imgHeight = 80
+const $imgsTheme = { "default": "buraco.gif", "active": "toupeira.gif", "dead": "morreu.gif" }
 $(document).ready(function() {
         fillboard();
         $("#btnPlay").click(function() {
@@ -21,9 +22,17 @@ function placeHolesBoard($level) {
     $("#board").empty()
     for ($i = 0; $i < Math.pow($level, 2); $i++) {
         $div = $("<div></div>"); //.attr("id", `mole_${$i+1}`);
-        $img = $("<img>").attr({ "src": "img/buraco.gif", "id": `mole_${$i+1}` });
+        $img = $("<img>").attr({ "src": `img/${$imgsTheme.default}`, "id": `mole_${$i+1}` });
+        $($img).click(function() { updateScore(this) });
         $($div).append($img);
         $("#board").append($div);
+    }
+}
+
+function updateScore($img) {
+    if ($($img).attr("src").search("toupeira") != -1) {
+        $("#score").text(parseInt($("#score").text()) + 1);
+        $($img).attr({ "src": `img/${$imgsTheme.dead}` })
     }
 }
 
@@ -32,7 +41,7 @@ function startGame() {
     $level = getLevel()
         // $level = $levels[$("#level").val()]
     $randNumber = getRandNumber(1, Math.pow($level, 2))
-    $(`#mole_${$randNumber}`).attr("src", "img/toupeira.gif")
+    $(`#mole_${$randNumber}`).attr("src", `img/${$imgsTheme.active}`)
 }
 
 function getRandNumber(min, max) {
